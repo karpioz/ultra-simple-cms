29f5478092b3ca0a19b2da2352ee97e52b9fa039
karpioz <karpioz@gmail.com>
2020-12-31 11:39:20 +0000

    hardcoded post view and routes added

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
+
+Route::get('/post', [App\Http\Controllers\PostController::class, 'show'])->name('post');
+
+// Route::get('/post', PostController@show)->name('post')
