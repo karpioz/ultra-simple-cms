8aa2b0cc59b71661cc2f6d3fa4a12319d83c2360
karpioz <karpioz@gmail.com>
2021-01-01 19:21:37 +0000

    posts display on homepage  added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+ 
+use App\Models\Post; 
 
 class HomeController extends Controller
 {
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $posts = Post::all();
+
+        return view('home', ['posts' => $posts]);
     }
 }
